fb6790b73bb6f1e00d7985edbcfb8886f44dd1ff
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-07-07 08:08:56 +0530

    fixed logo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import viteLogo from '/vite.svg'
+import logo from '/logo.svg'
 import Navigator from './Navigator'
 
 export default function Header() {
     return (
         <>
             <div className="px-16 flex justify-between w-full z-[100]">
-                <div>
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                <div className='logo-container'>
+                    <img src={logo} className="logo" alt="Logo" />
                 </div>
                 <div className='flex justify-between space-x-4'>
                     <Navigator>About</Navigator>
