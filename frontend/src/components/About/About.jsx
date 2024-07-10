d8fd7090d11cebacb66eeb6fe9b99016a90af681
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-07-10 23:19:15 +0530

    skills card

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -5,6 +5,7 @@ import BeachRight from "../../assets/Beach/BeachRight"
 import { useScroll } from "../Util/ScrollProvider"
 import Highlighter from "../Util/Highlighter"
 import DescLine from "./DescLine"
+import Skills from "../Skills/Skills"
 
 export default function About() {
     const { scrollVal, setScrollVal } = useScroll()
@@ -44,15 +45,15 @@ export default function About() {
                             <DescLine>
                                 Hi, I'm Himanshu Lilhore, a 2022
                                 <Highlighter bgColor="bg-sky-400">B. Tech graduate</Highlighter>
-                                and passionate 
+                                and passionate
                                 <Highlighter bgColor="bg-violet-400">Full-Stack Developer</Highlighter>
                             </DescLine>
                             <DescLine>Ujjain | Bhopal | Hyderabad</DescLine>
                             <DescLine>
-                                At 
+                                At
                                 <Highlighter bgColor="bg-green-500">Deloitte</Highlighter>
-                                , I have worked on web development stacks including 
-                                <Highlighter bgColor="bg-rose-400">MERN and Java</Highlighter> 
+                                , I have worked on web development stacks including
+                                <Highlighter bgColor="bg-rose-400">MERN and Java</Highlighter>
                                 in backend for health projects</DescLine>
                             <DescLine>Also worked on making internal tools (being used by team of 200+ people on regular basis)</DescLine>
                             <DescLine>Want to make a meaningful impact through software development and innovation.</DescLine>
@@ -61,7 +62,11 @@ export default function About() {
                 </div>
 
 
-                
+                <div className="z-[90] relative"
+                    style={{ top: - scrollVal * 2.1 }}
+                >
+                    <Skills></Skills>
+                </div>
             </div>
         </>
     )
