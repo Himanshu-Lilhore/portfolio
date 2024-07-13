c5c87109072fcc0093c85f25351a80ad338a858e
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-07-13 23:52:35 +0530

    minor changes

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,17 +1,21 @@
 import SocialPalette from "./SocialPalette";
+import ContactCard from "./ContactCard";
 
-
-export default function Contact () {
+export default function Contact() {
 
     return (
-        <div className="flex flex-col">
+        <div className="px-36 py-16 flex justify-around">
+            <div className="flex">
 
-            <div>
-                <SocialPalette />
+                <div>
+                    <SocialPalette />
+                </div>
+
+                <div className="">
+                    <ContactCard />
+                </div>
+
             </div>
-
-            <div></div>
-
         </div>
     )
 }
---
+++ frontend/src/components/Contact/ContactCard.jsx
@@ -0,0 +1,36 @@
+
+export default function ContactCard() {
+    return (
+        <div className="text-black text-xl border-2 border-black rounded-3xl py-3 px-10">
+            <div className="text-xl py-6 font-bold flex justify-center underline underline-offset-4">
+                Write a message or a feedback
+            </div>
+            <div className="flex justify-between py-4">
+                <label htmlFor="name">Name</label>
+                <input
+                    id="name"
+                    className="text-base rounded-md px-2"
+                    style={{ minWidth: '240px' }}
+                    placeholder="(optional)"
+                />
+            </div>
+            <div className="flex justify-between py-4">
+                <label htmlFor="message">Message</label>
+                <textarea
+                    id="message"
+                    className="text-base rounded-md px-2"
+                    style={{ minWidth: '240px' }}
+                    placeholder="Write a message..."
+                />
+            </div>
+            <div className="flex justify-center py-4">
+                <button
+                    className="rounded-full px-5 py-1"
+                    style={{ color: '#f48925', backgroundColor: 'black' }}
+                >
+                    SEND
+                </button>
+            </div>
+        </div>
+    )
+}
